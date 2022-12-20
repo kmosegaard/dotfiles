1a8aa5d7520cf343b955e6828034eb63c0822a87
Kristian Mosegaard <kristian@mosen.me>
2022-12-20 05:04:24 +0100

    nvim: setup netrw.

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -11,11 +11,18 @@ vim.opt.wrap = false
 vim.opt.termguicolors = true
 vim.opt.completeopt = 'menuone,noinsert,noselect'
 
+vim.g.netrw_keepdir = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 30
+
 local map = vim.api.nvim_set_keymap
 map('n', '<Space>', '', {})
 vim.g.mapleader = ' '
 
 options = { noremap = true }
+map('n', '<leader>dd', ':Lexplore %:p:h<cr>', {})
+map('n', '<leader>da', ':Lexplore<cr>', {})
+
 map('n', '<leader>ff', ':lua require("telescope.builtin").find_files()<cr>', options)
 map('n', '<leader>fd', ':lua require("telescope.builtin").git_files()<cr>', options)
 map('n', '<leader>fg', ':lua require("telescope.builtin").live_grep()<cr>', options)
